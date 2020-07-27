8a41c158e3dd50af3f837e67a383fa36d592b182
not ervin <notminin@pm.me>
2020-07-27 16:40:28 +0100

    cli.js - nab colorize.js lib from mininmobile/ttxt

--- cli.js
+++ cli.js
@@ -1,3 +1,5 @@
+const interpreter = new (require("./lib/flexscript"))();
+
 let args = process.argv.splice(2);
 
 if (args[0] == "--help" || args[0] == "-h") {
@@ -8,11 +10,36 @@ if (args[0] == "--help" || args[0] == "-h") {
 		"  [no args]\tstart interactive console",
 	].join("\n"));
 } else if (args[0]) {
+	// file-only imports
+	let fs = require("fs");
+
 	// launch interpreter into file
-	const interpreter = new (require("./lib/flexscript"))();
-	console.log("cum");
+	fs.readFile(args[0], (err, data) => {
+		if (err)
+			throw new Error(err);
+
+		let meta = interpreter.process(data);
+		interpreter.parse(data, meta);
+	});
 } else {
+	// console-only imports
+	let readline = require("readline");
+	let colorize = require("./lib/colorize");
+
 	// launch into interactive console
-	const interpreter = new (require("./lib/flexscript"))();
-	console.log(interpreter.fart);
+	interpreter.assign("exit", () => { process.exit(0) });
+
+	console.log("FLEXSCRIPT interactive console");
+	console.log("Copyleft zvava (@zvava@toot.cafe)\n");
+	ask();
+
+	function ask() {
+		let rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+		rl.question(colorize.blue("FS> "), (input) => {
+			rl.close();
+			interpreter.parseLine(input);
+			ask();
+		});
+	}
 }
